5e15f07e452c1132612ed283e5961cf71673e6ac
TheCicus <158371402+TheCicus@users.noreply.github.com>
2024-06-30 15:56:03 +0200

    Delete cicus_staff directory

--- cicus_staff/server.lua
+++
@@ -1,7 +0,0 @@
-
-ESX = exports["es_extended"]:getSharedObject()
-
-ESX.RegisterServerCallback('CICUS:STAFF:REQUESTGROUP', function(src, cb)
-    local xPlayer = ESX.GetPlayerFromId(src)
-    cb({group = xPlayer.getGroup(),  name = GetPlayerName(src)})
-end)
